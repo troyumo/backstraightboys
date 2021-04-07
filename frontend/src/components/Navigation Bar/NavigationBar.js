b30f5a74b93fae914942615d8619de68bc3ae136
Troy Umolac <65733169+troyumo@users.noreply.github.com>
2021-04-06 21:52:49 -0400

    get rid of files that weren't being used and add resources page

--- frontend/src/components/Navigation Bar/NavigationBar.js
+++ frontend/src/components/Navigation Bar/NavigationBar.js
@@ -6,7 +6,7 @@ export default function Navi() {
     return (
         <div className="topnav">
             <a className="active" href="/"><img src={images.logo} alt="BackStraight Boys" width={162} height={19}/></a>
-            <a href="log.html">Log</a>
+            <a target="log" href={"http://localhost:8000/"} >Log</a>
             <a href="resources.html">Resources</a>
             <a href="profile.html"><img src={images.user} alt="User" width={20} height={20}/></a>
         </div>
